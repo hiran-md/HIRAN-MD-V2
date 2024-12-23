412c142d2d6202f1e90bdb7e3a55ffc2f3667850
Hiranya Sathsara <hiranmdbot@gmail.com>
2024-12-23 15:06:30 +0530

    Update list.js

--- plugins/list.js
+++ plugins/list.js
@@ -13,7 +13,6 @@ cmd({
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
     
-}
 
 let menu = {
 main: '',
