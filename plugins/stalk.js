daba3a2d3d8eb63234b25eefdf28acb66a90e052
Hiranya Sathsara <hiranmdbot@gmail.com>
2024-12-24 14:15:53 +0530

    Create stalk.js

---
+++ plugins/stalk.js
@@ -0,0 +1,40 @@
+const axios = require('axios');
+const config = require('../config');
+const { cmd, commands } = require('../command');
+
+cmd({
+    pattern: "githubstalk",
+    desc: "Fetch detailed GitHub user profile including profile picture.",
+    category: "search",
+    react: "🫴🏻",
+    filename: __filename
+},
+async (conn, mek, m, { from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply }) => {
+    try {
+        const username = args[0];
+        if (!username) {
+            return reply("Please provide a GitHub username.");
+        }
+        const apiUrl = `https://api.github.com/users/${username}`;
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        let userInfo = `❤️ ʜɪʀᴀɴ-ᴍᴅ ᴍᴀᴅᴇ ʙʏ ʜɪʀᴀɴʏᴀ ꜱᴀᴛʜꜱᴀʀᴀ ❤️
+        
+👤 *Username*: ${data.name || data.login}
+🔗 *Github Url*:(${data.html_url})
+📝 *Bio*: ${data.bio || 'Not available'}
+🏙️ *Location*: ${data.location || 'Unknown'}
+📊 *Public Repos*: ${data.public_repos}
+👥 *Followers*: ${data.followers} | Following: ${data.following}
+📅 *Created At*: ${new Date(data.created_at).toDateString()}
+🔭 *Public Gists*: ${data.public_gists}
+
+  ʜɪʀᴀɴ-ᴍᴅ
+`;
+          const sentMsg = await conn.sendMessage(from,{image:{url: config.ALIVE_IMG },caption: userInfo },{quoted:mek })
+    } catch (e) {
+        console.log(e);
+        reply(`error: ${e.response ? e.response.data.message : e.message}`);
+    }
+});
